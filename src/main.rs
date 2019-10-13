d8cacca9e5ac874bb2b6f49bd68932e16e43483c
bbdare <dajtg6@gmail.com>
2019-10-12 19:14:14 -0500

    Comments, Initial Cleanup

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,7 @@ impl Trail {
         }
     }
 }
+//Fields and Constructor of falling trails
 
 fn main() -> Result<(), ErrorKind> {
     let cursor = cursor();
@@ -31,17 +32,27 @@ fn main() -> Result<(), ErrorKind> {
 
     cursor.hide()?;
 
+    let delay = 45;
+    let colorset: Vec<Color> = vec![Color::Green];
+    let spawn_rate: f64 = 0.01;
+    //Values intended to use arguments go here
+
     let mut trails: Vec<Trail> = Vec::new();
     let mut rng = rand::thread_rng();
 
     loop {
         let mut removal_flags: Vec<usize> = Vec::new();
+        //Indexes of the trail are fed here to be removed at the end of each draw
+
         let (x, y) = term.size()?;
         for i in 0..x - 1 {
-            if rng.gen::<f64>() > 0.99 {
+            if rng.gen::<f64>() < spawn_rate {
                 trails.push(Trail::new(i.into(), rng.gen_range(5, y - 10)));
             }
         }
+        //Iterate across x every loop,
+        //each coordinate has a chance (variable spawn_rate) of spawning a new trail each update.
+        //TODO: consider trail length as argument field too?
 
         for i in 0..trails.len() {
             let head_y = trails[i].y_head;
@@ -50,42 +61,61 @@ fn main() -> Result<(), ErrorKind> {
                 tail_y = trails[i].y_head as i32 - trails[i].y_tail as i32;
             }
             let x = trails[i].x;
+            //Initial lets to avoid repeatedly calling vector element
+            //Note that majority of if statements are to avoid panic from invalid coordinates.
 
             if head_y == y {
                 trails[i].printing = false;
             }
+            //Mark path to only be cleaned up by tail.
 
             if trails[i].printing {
                 if head_y >= 1 {
                     cursor.goto(x, head_y - 1)?;
-                    let line = style(format!("{}", trails[i].last_char)).with(Color::Green);
+                    let line = style(format!("{}", trails[i].last_char))
+                        .with(colorset[rng.gen_range(0, colorset.len())]);
                     print!("{}", line);
                 }
-                cursor.goto(x, head_y)?;
-                let rnchara = char::from_u32(rng.gen_range(65296,65371)).unwrap();
-                let rncharb = char::from_u32(rng.gen_range(12449,12542)).unwrap();
-                
-                let rnchar:char;
+                //Color previous line to random element of set
+                //TODO: see if possible to change color without saving last value (get_character?)
+
+                let rnchara = char::from_u32(rng.gen_range(65296, 65371)).unwrap();
+                let rncharb = char::from_u32(rng.gen_range(12449, 12542)).unwrap();
+                let rnchar: char;
                 if rng.gen::<f64>() < 0.5 {
                     rnchar = rnchara;
                 } else {
                     rnchar = rncharb;
                 }
+                //Random Character Generation
+                //Current Set A: [65296,65370]: Fullwidth Letters+Numbers
+                //Current Set B: [12449, 12541]: Fullwidth Katakana
+                //TODO: Improve selection for multiple character ranges (vector of chars?)
+                //TODO: Allow custom charsets via argument
+
+                cursor.goto(x, head_y)?;
                 let line = style(format!("{}", rnchar)).with(Color::White);
                 print!("{}", line);
-                trails[i].last_char=rnchar;
+                trails[i].last_char = rnchar;
+                //Print random character to head location, save character for later update
+                //TODO: see last_char draw section
+                //TODO?: relegate this printing to struct method
             }
 
             trails[i].y_head += 1;
 
             if tail_y != -1 {
-                cursor.goto(x, (tail_y % 65535) as u16)?;
+                cursor.goto(x, tail_y as u16)?;
                 print!(" ");
             }
+            //Tail clearing of lines
 
             if (trails[i].y_head as i32 - trails[i].y_tail as i32) == y as i32 {
                 removal_flags.push(i);
             }
+            //Buffer indices of completed lines to be removed after loop
+            //Immediate removal would require modification of i
+            //TODO: Possibly Unnecessary, as lines should finish in order of creation
         }
 
         removal_flags.sort();
@@ -93,7 +123,10 @@ fn main() -> Result<(), ErrorKind> {
         for i in 0..removal_flags.len() {
             trails.remove(i);
         }
-        
-        thread::sleep(time::Duration::from_millis(20));
+        //Remove Flagged lines
+        //TODO: Possible modification to simply clear index 0 of trails n times,
+        //where n is the amount of lines finished.
+
+        thread::sleep(time::Duration::from_millis(delay));
     }
 }
